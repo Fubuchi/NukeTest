675abc621d13e79990e9806231d975b95066497e
Fubuchi <haiuit9795@gmail.com>
2019-07-27 11:37:21 +0700

    Init source, test and build project

---
+++ source/app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using library;
+
+namespace app
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Math: ");
+      Console.WriteLine("5 + 6 = {0}", library.Math.add(5, 6));
+      Console.WriteLine("5 - 6 = {0}", library.Math.subtract(5, 6));
+    }
+  }
+}
---
+++ source/library/Math.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace library
+{
+  public class Math
+  {
+    public static int add(int left, int right) => left + right;
+    public static int subtract(int left, int right) => left - right;
+  }
+}
